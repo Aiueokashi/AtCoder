f712a8037585fee8f0f77f7740979cf1918f0bad
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2020-11-28 22:18:55 +0900

    Update Define.cpp

--- PastDefine/Define.cpp
+++ PastDefine/Define.cpp
@@ -1,17 +1,17 @@
-/*#define QLUBinarySearch(type,vec,k)  sort(all(vec)); \
+#define QLUBinarySearch(type,vec,k)  sort(all(vec)); \
 int key = k; \
 auto iter = type(all(vec),key);\
 if (iter != vec.end() && *iter == key) {\
         cout << key << " is found at " << distance(vec.begin(), iter) << "\n";\
     } else {\
         cout << key << "is NOT found." << "\n";\
-    }*/
+    }
     
-/*#define QBinarySearch(vec, fkey, ekey) sort(all(vec));\
+#define QBinarySearch(vec, fkey, ekey) sort(all(vec));\
 for (int key = fkey; key <= ekey; ++key) {\
         if (binary_search(vec.begin(), vec.end(), key)) {\
            print(key << ":" << "true") \
         } else {\
             print(key << ":" << "false")\
         }\
-    }*/
+    }
